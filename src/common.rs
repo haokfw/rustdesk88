52f2fd58e573bea7940f3d099a02782be72a99b5
haokfw <65384256+haokfw@users.noreply.github.com>
2025-10-20 07:53:15 +0800

    Update URL to include port number

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://39.108.103.48".to_owned()
+    "http://39.108.103.48:21114".to_owned()
 }
 
 #[inline]
